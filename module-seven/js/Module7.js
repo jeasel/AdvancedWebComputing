c78427c8ecd6f8ea2ad55d1df1e5fb58c5cb0840
jeasel <cjeasel@yahoo.com>
2013-08-27 03:54:22 +1200

    Update Module7.js

--- module-seven/js/Module7.js
+++ module-seven/js/Module7.js
@@ -1,14 +1,57 @@
 $(document).ready(function(){
+	$('#movie').keypress(function(event){
+		if(event.which == 13){
+			var searchmovies = $('#movie').val();
+			if(searchmovies == ""){
+				alert("Please input the required field.");
+			}
+			$(function(){
+				$('#result1').html("");
+				$('#result1').append('<p class="text-info">Search Result for: '+searchmovies+ '</p>');
+				var url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json';
+				$.ajax({
+					url: url,
+					data: {
+						q: searchmovies,
+						apiKey: 's4g4bxznr29fucmhs5fvjx3u'
+					},
+					dataType: 'jsonp',
+					success: showmovies
+				});
+				function showmovies(response){
+					$('.MovieContainer').html("");
+					for(i=0;i<response.movies.length;i++){
+						var movie = response.movies[i];
+						var synop = movie.synopsis;
+						if(synop == ""){
+							synop = '<h3 style="text-align: center;">No Available Synopsis</h3>';
+							$('.MovieContainer').append('<div class="MovieHolder">'+'Year: '+movie.year+'<div class="Thumb">'+'<img src="' 
+							+movie.posters.thumbnail+'"/>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
+							+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>');
+						
+						}else{
+							$('.MovieContainer').append('<div class="MovieHolder">'+'Year: '+movie.year+'<div class="Thumb">'+'<img src="' 
+							+movie.posters.thumbnail+'"/>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
+							+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>');
+						
+						}
+					}
+						var movieslength = response.movies.length;
+						$('#result2').html("");
+						$('#result2').append('<p class="text-info">Total number of results found: '+movieslength+'</p>');
+				}
+			});
+				searchmovies = $('#movie').val("");
+		}
+	});
 	$('#send').click(function(){
-
 		var searchmovies = $('#movie').val();
 		if(searchmovies == ""){
-			alert("Please input the required field!");
+			alert("Please input the required field.");
 		}
-			$(function(){
-			
+		$(function(){
 			$('#result1').html("");
-			$('#result1').append('<p class="text-info">Search Result for: ' +searchmovies+ '</p>');
+			$('#result1').append('<p class="text-info">Search Result for: '+searchmovies+'</p>');
 			var url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json';
 			$.ajax({
 				url: url,
@@ -16,10 +59,10 @@ $(document).ready(function(){
 					q: searchmovies,
 					apiKey: 's4g4bxznr29fucmhs5fvjx3u'
 				},
-					dataType: 'jsonp',
-					success: showMovies
+				dataType: 'jsonp',
+				success: showmovies
 			});
-			function showMovies(response){
+			function showmmovies(response){
 				$('.MovieContainer').html("");
 				for(i=0;i<response.movies.length;i++){
 					var movie = response.movies[i];
@@ -30,25 +73,18 @@ $(document).ready(function(){
 						+movie.posters.thumbnail+'"/>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
 						+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>');
 						
-					}else{	
-					
-					$('.MovieContainer').append('<div class="MovieHolder">'+'<div class="Thumb">'+'Year: '+movie.year+'<img src="' 
+					}else {
+						$('.MovieContainer').append('<div class="MovieHolder">'+'Year: '+movie.year+'<div class="Thumb">'+'<img src="' 
 						+movie.posters.thumbnail+'"/>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
 						+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>');
+						
 					}
 				}
-					var moviesLength = response.movies.length;
-					$('#result2').html("");
-					$('#result2').append('<p class="text-info">Total result found: '+moviesLength+'</p>');
+				var movieslength = response.movies.length;
+				$('#result2').html("");
+				$('#result2').append('<p class="text-info">Total number of results found: '+movieslength+'</p>');
 			}
-				
-			});
-				$( '#moviesform' ).each(function(){
-					this.reset();
-				});
-			
 		});
-		
-
+			searchmovies = $('#movie').val("");
+	});
 });
-
