0b7026037b8a9135304140c192cc29c3823b37b6
jeasel <cjeasel@yahoo.com>
2013-08-26 17:55:44 +1200

    Update Module7.js

--- module-seven/js/Module7.js
+++ module-seven/js/Module7.js
@@ -28,16 +28,14 @@ $(document).ready(function(){
 					if(synop == ""){
 						synop = '<h3 style="text-align: center;">No Available Synopsis</h3>';
 						$('.MovieContainer').append('<div class="MovieHolder">'+'<div class="Thumb">'+'<img src="' 
-						+movie.posters.thumbnail+'"/>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
-						+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>'+'<div class="Year">'+'<p>Year: ' 
-						+movie.year+ '</p>'+'</div>'+'</div>'+'<div class="clear">'+'</div>');
+						+movie.posters.thumbnail+'"/>'+'<p>Year: '+movie.year+'</p>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
+						+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>');
 						
 					}else{	
 					
 					$('.MovieContainer').append('<div class="MovieHolder">'+'<div class="Thumb">'+'<img src="' 
-					+movie.posters.thumbnail+'"/>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
-					+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>'+'<div class="Year">'+'<p>Year: ' 
-					+movie.year+ '</p>'+'</div>'+'</div>'+'<div class="clear">'+'</div>');
+					+movie.posters.thumbnail+'"/>'+'<p>Year: '+movie.year+'</p>'+'</div>'+'<div class="Title">'+'<p class="MovieTitle">' 
+					+movie.title+ '</p>'+'<div class="synopsis">'+'<p>'+synop+'</p>'+'</div>'+'</div>');
 					}
 				}
 					var moviesLength = response.movies.length;
